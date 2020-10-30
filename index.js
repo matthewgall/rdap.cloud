6def78b0a45ad11713781074be79a68a44cebe90
Matthew Gall <git@matthewgall.com>
2020-10-30 23:02:11 +0000

    Added Prometheus metrics gathering

--- index.js
+++ index.js
@@ -23,6 +23,59 @@ const headers = {
     }
 }
 
+async function metrics(request) {
+    let ts = Math.round((new Date()).getTime() / 1000)
+    let count = 0
+    let expiry = {
+        '3600': 0,
+        '21600': 0,
+        '43200': 0,
+        '86400': 0,
+        '172800': 0
+    }
+    let cursor = ''
+    let complete = false
+
+    while (complete !== true) {
+        let data = await KV.list({
+            "cursor": cursor
+        })
+        count = count + data['keys'].length
+
+        let delta = 0
+
+        for (let key of data['keys']) {
+            delta = key['expiration'] - ts
+
+            for (let exp in expiry) {
+                if (delta < exp) {
+                    expiry[exp] = expiry[exp] + 1
+                }
+            }
+        }
+
+        complete = data['list_complete']
+        if (complete !== true) {
+            cursor = data.cursor
+        }
+    }
+
+    return new Response(`# HELP rdap_keys_cached_total The total number of cached RDAP lookups
+# TYPE rdap_keys_cached_total counter
+rdap_keys_cached_total ${count}
+# HELP rdap_keys_expires_total The total number of cached RDAP lookups expiring in a set timeframe
+# TYPE rdap_keys_expires_total histogram
+rdap_keys_expires_total{handler="/metrics",le="3600"} ${expiry['3600']}
+rdap_keys_expires_total{handler="/metrics",le="21600"} ${expiry['21600']}
+rdap_keys_expires_total{handler="/metrics",le="43200"} ${expiry['43200']}
+rdap_keys_expires_total{handler="/metrics",le="86400"} ${expiry['86400']}
+`, {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+}
+
 async function services(request) {
     let rdap = new Rdap()
     let data = await rdap.getServices()
