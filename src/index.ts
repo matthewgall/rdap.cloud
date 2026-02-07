5b887433a01052344ba04e7634cf863af2a0bb7e
Matthew Gall <me@matthewgall.dev>
2026-02-07 02:14:05 +0000

    It's 2026, act like it!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2020 - 2023 Matthew Gall
+ * Copyright 2020 - 2026 Matthew Gall
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
